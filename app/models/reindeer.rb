be05fac2aae9cf0a30fe1bb1e1dfe14a356671d0
vagrant <vagrant@lighthouse-labs>
2016-07-14 15:35:20 +0000

    Reindeer edit

--- app/models/reindeer.rb
+++ app/models/reindeer.rb
@@ -2,3 +2,11 @@ class Reindeer < ActiveRecord::Base
   has_many :gifts
   has_many :children
 end
+
+# create_table "reindeers", force: :cascade do |t|
+#   t.string   "name"
+#   t.string   "email"
+#   t.string   "mobile"
+#   t.datetime "created_at"
+#   t.datetime "updated_at"
+# end
