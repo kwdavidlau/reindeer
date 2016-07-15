f86dd8dabb4918059407d43570fc9087c900bd9d
Tobin Elavathil <tobin.elavathil@gmail.com>
2016-07-15 08:29:19 -0400

    Reindeer view compete, bug remain in Items to be delivered view

--- app/actions.rb
+++ app/actions.rb
@@ -3,87 +3,96 @@
 # -----------------------
 
 get '/' do
-  erb :index
+    erb :index
 end
 
 get '/elves' do
-  erb:'/elves/index'
+    erb:'/elves/index'
 end
 
 # Reindeer page showing their deliveries`-------------
 
 get '/reindeer' do
-  @deliveries = Delivery.all
-  @reindeers = Reindeer.all
-  erb :'reindeer/index'
+    @deliveries = Delivery.all
+    @reindeers = Reindeer.all
+    erb :'reindeer/index'
 end
 
 # Children page showing the status of their delivery------------
 
 get '/children' do
-  @children = Child.all
-  erb :'/children/index'
+    @children = Child.all
+    erb :'/children/index'
 end
 
 #----- get information
 # -------------------
 
 post '/children' do
-  @child = Child.new(
-    name: params[:name],
-    age: params[:age],
-    address: params[:address],
-    received: false
-  )
-  @child.save
-  redirect :'/elves'
+    @child = Child.new(
+        name: params[:name],
+        age: params[:age],
+        address: params[:address],
+        received: false
+    )
+    @child.save
+    redirect :'/elves'
 end
 
 post '/reindeer' do
-  @reindeer = Reindeer.new(
-    name: params[:name],
-    email: params[:email],
-    mobile: params[:mobile]
-  )
-  @reindeer.save
-  redirect :'/elves'
+    @reindeer = Reindeer.new(
+        name: params[:name],
+        email: params[:email],
+        mobile: params[:mobile]
+    )
+    @reindeer.save
+    redirect :'/elves'
 end
 
 post '/gifts' do
-  @delivery = Delivery.new
-  @gift = Gift.new(
-    name_of_gift: params[:gift],
-    gift_value: params[:gift_value]
-  )
-  @gift.save
-  @delivery.gift = @gift
-  @delivery.save
-  redirect :'/elves'
+    @delivery = Delivery.new
+    @gift = Gift.new(
+        name_of_gift: params[:gift],
+        gift_value: params[:gift_value]
+    )
+    @gift.save
+    @delivery.gift = @gift
+    @delivery.save
+    redirect :'/elves'
 end
 
 # elves/show to show all the children, reindeer, and gifts ---------
 
-
 get '/elves/show' do
-  @gifts = Gift.all
-  @unassigned_deliveries = Delivery.where(reindeer_id: nil)
-  @reindeers = Reindeer.all
-  @children = Child.all
+    @gifts = Gift.all
+    @unassigned_deliveries = Delivery.where(reindeer_id: nil)
+    @reindeers = Reindeer.all
+    @children = Child.all
 
-  #assigning the gifts to children
-  @children.each do |child|
-    @gifts.each do |gift|
-      # binding.pry
-      gift.child = child if (child.gift_id.nil? && gift.child.nil?)
+    # assigning the gifts to children
+    @children.each do |child|
+        @gifts.each do |gift|
+            # binding.pry
+            gift.child = child if child.gift_id.nil? && gift.child.nil?
+        end
     end
-  end
 
+    # assigning the reindeer to gifts
+    reindeer_enum = @reindeers.cycle
 
-  #assigning the reindeer to gifts
-  reindeer_enum = @reindeers.cycle
+    @unassigned_deliveries.each do |delivery|
+        reindeer_enum.next.deliveries << delivery unless Reindeer.all.empty?
+    end
+    erb :'/elves/show'
+end
 
-  @unassigned_deliveries.each do |delivery|
-    reindeer_enum.next.deliveries << delivery unless Reindeer.all.empty?
-  end
-  erb :'/elves/show'
+post '/deliveries' do
+    d = Delivery.find(params[:id])
+    if params.key?('delivered')
+        d.delivery_status = params[:delivered] == "on" ? true : false
+    else
+        d.time_to_deliver = params[:date]
+    end
+    d.save
+    redirect :'/reindeer'
 end
--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,23 +1,29 @@
-$(document).ready(function(){
- var i=1;
+$(document).ready(function() {
+    var i = 1;
 
- // parallax init
- $('.parallax').parallax();
+    // parallax init
+    $('.parallax').parallax();
 
- //select init
- $('select').material_select();
+    //select init
+    $('select').material_select();
 
-$("#add_row").click(function(){
- $('#addr'+i).html("<td>"+ (i+1) +"</td><td><input name='name"+i+"' type='text' placeholder='Name' class='form-control input-md'  /> </td><td><input  name='mail"+i+"' type='text' placeholder='Mail'  class='form-control input-md'></td><td><input  name='mobile"+i+"' type='text' placeholder='Mobile'  class='form-control input-md'></td>");
+    // datepicker init
+    $('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15 // Creates a dropdown of 15 years to control year
+  });
 
- $('#tab_logic').append('<tr id="addr'+(i+1)+'"></tr>');
- i++;
-});
-$("#delete_row").click(function(){
-  if(i>1){
-$("#addr"+(i-1)).html('');
-i--;
-}
-});
+    $("#add_row").click(function() {
+        $('#addr' + i).html("<td>" + (i + 1) + "</td><td><input name='name" + i + "' type='text' placeholder='Name' class='form-control input-md'  /> </td><td><input  name='mail" + i + "' type='text' placeholder='Mail'  class='form-control input-md'></td><td><input  name='mobile" + i + "' type='text' placeholder='Mobile'  class='form-control input-md'></td>");
+
+        $('#tab_logic').append('<tr id="addr' + (i + 1) + '"></tr>');
+        i++;
+    });
+    $("#delete_row").click(function() {
+        if (i > 1) {
+            $("#addr" + (i - 1)).html('');
+            i--;
+        }
+    });
 
 });
