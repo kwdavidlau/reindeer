3117377c31720578d56d7691ed8ae74785095736
Tobin Elavathil <tobin.elavathil@gmail.com>
2016-07-15 16:01:41 -0400

    Hero page and Twilio-rb gem integration

--- services/twilio.rb
+++ services/twilio.rb
@@ -1,3 +1,11 @@
-require 'rubygems'
 require 'twilio-ruby'
 require 'sinatra'
+
+post '/receive_sms' do
+  content_type 'text/xml'
+  response = Twilio::TwiML::Response.new do |r|
+    r.Message 'Test received message'
+  end
+
+  response.to_xml
+end
